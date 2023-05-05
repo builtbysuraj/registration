6c0ba1a362ffc1fe2b0dac1fd8ffe78c4c81646f
builtbysuraj <surajgupta3553@gmail.com>
2023-05-05 22:12:32 +0530

    Create Home page and Add favicon

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,26 @@ export default function Home() {
   return (
     <div className="home">
       <div className="home-wrapper">
-        <h1>This is Home page</h1>
+        <div className="details">
+          <div className="heading">User Details</div>
+          <div className="name">
+            <span>Name: </span>
+            <em>MyName</em>
+          </div>
+          <div className="email">
+            <span>Email: </span> <em>example@gmail.com</em>
+          </div>
+          <button className="log-out">Log out</button>
+        </div>
+        <form>
+          <div className="form-heading">Change password</div>
+          <input type="password" placeholder="Old password" />
+          <input type="password" placeholder="New password" />
+          <input type="password" placeholder="Re-enter new password" />
+          <button className="submit-btn" type="submit">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
